11bf86331c45ec78500a9913378f245b74a85f93
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-04-27 15:02:21 +0600

    dynamic products load, pagination added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,6 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useCart from "../../hooks/useCart";
-import useProducts from "../../hooks/useProducts";
 import { setCartData } from "../../utilities/cart-db";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
@@ -8,8 +8,25 @@ import styles from "./Shop.module.css";
 
 const Shop = () => {
     const navigate = useNavigate();
-    const [products] = useProducts(); //custom hooks
+    const [products, setProducts] = useState([]); //custom hooks
     const [cart, setCart] = useCart(products); //custom hooks
+    const [pages, setPages] = useState(0);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [productsPerPage, setProductsPerPage] = useState(12);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/productsCount?productsPerPage=${productsPerPage}`)
+            .then((response) => response.json())
+            .then((data) => setPages(data.pagesCount));
+    }, [productsPerPage]);
+
+    useEffect(() => {
+        fetch(
+            `http://localhost:5000/products?currentPage=${currentPage}&&productsPerPage=${productsPerPage}`
+        )
+            .then((response) => response.json())
+            .then((data) => setProducts(data));
+    }, [currentPage, productsPerPage]);
 
     //Add to cart event handler
     const addToCartHandler = (product) => {
@@ -38,7 +55,21 @@ const Shop = () => {
                     ))}
                 </div>
                 <div className={styles.pagination}>
-
+                    {[...Array(pages).keys()].map((number) => (
+                        <button className={number === currentPage ? styles.active : ''} key={number} onClick={() => setCurrentPage(number)}>
+                            {number + 1}
+                        </button>
+                    ))}
+                    <select
+                        defaultValue="12"
+                        onChange={(event) => setProductsPerPage(event.target.value)}
+                    >
+                        <option value="9">9</option>
+                        <option value="12">12</option>
+                        <option value="15">15</option>
+                        <option value="18">18</option>
+                        
+                    </select>
                 </div>
             </div>
             <div className={styles.orderSummeryContainer}>
